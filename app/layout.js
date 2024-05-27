952f925353da29e3536afd9d7f059085caad6dfd
SudeepMishra <sudeepm0599@gmail.com>
2024-05-27 17:52:25 +0545

    ENV

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import GoogleAdsense from "@/components/GoogleAds";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <GoogleAdsense pId={process.env.PID}/>
+        {children}
+        </body>
     </html>
   );
 }
